729bf17f7ecf8ff5f038d2d02c0a5873deaef74d
DracTheDino <sidtrip06@gmail.com>
2021-10-22 11:01:18 +0530

    Add info to About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,29 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <h1 className="text-center text-1xl md:text-4xl pt-3 font-italic text-white">
+    <div className="text-center">
+      <h1 className="text-1xl md:text-4xl pt-3 font-semibold text-white">
         About Me
       </h1>
+      <p className="text-xl pt-5 text-white font-semibold px-16">
+        Hobbies: Programming, motorsports, 3D modelling, cars, tech, music and
+        TV 💻. Preferably all at once 🤔. <br />
+        <br />
+        📖 High school student. <br />
+        <br />
+        🌱 Currently in love with Blender. <br />
+        📘 Currently making high poly realistic cars in Blender. <br />
+        📔 Software of choice: VSCode/IntelliJ IDEA/Blender. <br />
+        🖥️ Operating system: Windows 11 (+ WSL2, sometimes). <br />
+        🎵{" "}
+        <a
+          href="https://open.spotify.com/playlist/16c8EwGMSEp9NSRW8uZOSL?si=7e55cb3e583649eb"
+          target="_blank"
+          className="text-green-300 italic underline hover:bg-green-600 transition duration-500 transform"
+        >
+          My Spotify playlist.
+        </a>
+      </p>
     </div>
   );
 }
