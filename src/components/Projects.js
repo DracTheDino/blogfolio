62dbd0feda82f58759c5bb09ad4772169db86bf8
Siddharth <46294577+DracTheDino@users.noreply.github.com>
2021-10-24 16:28:39 +0530

    Refactor App.js, add Projects.js and ProjectCard.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,18 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 
-function Showcase() {
+function Projects() {
     return (
         <div className="text-center">
+            <h1 className="text-3xl md:text-4xl pt-3 font-semibold text-white">
+                Projects
+            </h1>
+            <div className="px-50">
+                <ProjectCard />
+            </div>
 
         </div>
     );
 }
 
-export default Showcase;
+export default Projects;
